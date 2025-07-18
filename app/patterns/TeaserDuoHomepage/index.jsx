61d5281bbb45b846d6629af392ce8c4d73b25bd9
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-07-18 17:42:50 +0200

    [NC-008] wip

--- app/patterns/TeaserDuoHomepage/index.jsx
+++ app/patterns/TeaserDuoHomepage/index.jsx
@@ -2,12 +2,12 @@ import './teaserDuoHomepage.scss';
 
 export function TeaserDuoHomepage({teaserImageLeft, teaserImageRight, content}) {
   return (
-    <div className="teaser-duo">
-      <div className="teaser-duo__image">
+    <div className="teaser-duo-homepage">
+      <div className="teaser-duo-homepage__image">
         <img src={teaserImageLeft} alt="" />
         <img src={teaserImageRight} alt="" />
       </div>
-      <div className="teaser-duo__content">
+      <div className="teaser-duo-homepage__content">
         <p>{content}</p>
       </div>
     </div>
