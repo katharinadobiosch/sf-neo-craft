85b42a3bb13d1622a8f7ee43aaac21440b374f72
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-07-25 14:09:18 +0200

    [NC-007] add size to productImage on PDP

--- app/patterns/ProductDetailInformation/ProductImage.jsx
+++ app/patterns/ProductDetailInformation/ProductImage.jsx
@@ -14,9 +14,11 @@ export function ProductImage({image}) {
       <Image
         alt={image.altText || 'Product Image'}
         // aspectRatio="1/1"
+        aspectRatio=""
         data={image}
         // key={image.id}
         // sizes="(min-width: 45em) 50vw, 100vw"
+        sizes=""
       />
       <Image
         alt={image.altText || 'Product Image'}
