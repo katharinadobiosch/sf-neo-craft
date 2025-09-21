37f977abdd8393b9095b6dc304c2bb70adfcfd06
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-09-21 19:03:55 +0200

    add materials

--- app/routes/highlights.tsx
+++ app/routes/highlights.tsx
@@ -1,7 +1,6 @@
-// app/routes/highlights.tsx
 import Highlights, {loader as highlightsLoader} from '../patterns/Highlights';
 
-export const loader = highlightsLoader; // <— wichtig
+export const loader = highlightsLoader;
 export default function HighlightsPageWithProps() {
   return <Highlights />;
 }
