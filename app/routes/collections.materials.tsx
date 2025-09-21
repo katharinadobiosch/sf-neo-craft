1b09722bad94942bbb8d0ff4d7c7e6582cf538f0
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-09-21 19:34:06 +0200

    fix materials page

--- app/routes/collections.materials.tsx
+++ app/routes/collections.materials.tsx
@@ -2,7 +2,5 @@ import Materials, {loader as materialsLoader} from '../patterns/Materials';
 
 export const loader = materialsLoader;
 export default function MaterialsPageWithProps() {
-  return;
-
-  <Materials />;
+  return <Materials />;
 }
