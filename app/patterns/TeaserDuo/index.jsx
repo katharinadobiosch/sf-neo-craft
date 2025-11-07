688acc4e055095e4918c0a6320635408c74da753
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-11-07 16:14:59 +0100

    [pdp] add TeaserDuo to bottom on pdp

--- app/patterns/TeaserDuo/index.jsx
+++ app/patterns/TeaserDuo/index.jsx
@@ -1,20 +1,45 @@
-export function TeaserDuo({teaserImageLeft, teaserImageRight, content}) {
-  const renderImage = (image) => {
-    if (typeof image === 'string') {
-      return <img src={image} alt="" />;
-    }
-    return image;
+import React from 'react';
+
+export function TeaserDuo({
+  teaserImageLeft,
+  teaserImageLeftHover,
+  teaserImageRight,
+  teaserImageRightHover,
+  content,
+}) {
+  const renderImagePair = (image, hoverImage) => {
+    const isString = typeof image === 'string';
+    const baseImg = isString ? <img src={image} alt="" /> : image;
+    const hoverImg =
+      hoverImage && typeof hoverImage === 'string' ? (
+        <img src={hoverImage} alt="" className="hover-img" aria-hidden="true" />
+      ) : (
+        hoverImage
+      );
+
+    return (
+      <div className="hover-wrap">
+        {baseImg}
+        {hoverImg}
+      </div>
+    );
   };
 
   return (
     <div className="teaser-duo">
       <div className="teaser-duo__images">
-        <div className="teaser-duo__image">{renderImage(teaserImageLeft)}</div>
-        <div className="teaser-duo__image">{renderImage(teaserImageRight)}</div>
+        <div className="teaser-duo__image">
+          {renderImagePair(teaserImageLeft, teaserImageLeftHover)}
+        </div>
+        <div className="teaser-duo__image">
+          {renderImagePair(teaserImageRight, teaserImageRightHover)}
+        </div>
       </div>
-      <div className="teaser-duo__content">
-        <p>{content}</p>
-      </div>
+      {content && (
+        <div className="teaser-duo__content">
+          <p>{content}</p>
+        </div>
+      )}
     </div>
   );
 }
