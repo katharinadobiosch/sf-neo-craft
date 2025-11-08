e889700f207cd2f40a200c0f446d56f98d0d3df2
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-11-08 18:40:14 +0100

    [pdp] fix bespoke page

--- app/patterns/Bespoke/index.jsx
+++ app/patterns/Bespoke/index.jsx
@@ -13,11 +13,11 @@ export default function Bespoke() {
         right={bespokeImageRight}
         content="NEO CRAFT is a Berlin-based furniture label founded by designer Sebastian Scherer in 2015 , dedicated to blending traditional craftsmanship with modern production techniques. The label challenges conventional ideas about materials, form, and function, reinterpreting them to create innovative and unexpected designs. At the heart of NEO/CRAFT lies a distinctive approach to material exploration and development processes. While the current focus is on metal, glass, and wood, the pursuit of new directions and fresh perspectives remains a fundamental part of NEO/CRAFT's ethos."
       />
-      <HeroSplit
+      {/* <HeroSplit
         left={bespokeImageLeft}
         right={bespokeHeroImage}
         content="GOBA is about simplicity with character—a piece that invites interaction and creates presence without overwhelming the space."
-      />
+      /> */}
     </>
   );
 }
