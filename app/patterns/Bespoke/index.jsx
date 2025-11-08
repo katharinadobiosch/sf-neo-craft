426e8836967c7f7efabd3d3ba27559f936fddfcd
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-11-08 21:14:33 +0100

    [pdp] add stryling to bespoke

--- app/patterns/Bespoke/index.jsx
+++ app/patterns/Bespoke/index.jsx
@@ -18,23 +18,32 @@ import teaserDuoRightBottom from './NC_POLY_OBLONG_SATINGLASS_BRONZE_72dpi.png';
 export default function Bespoke() {
   return (
     <div className="bespoke">
-      <TeaserDuo
-        left={teaserDuoLeft}
-        right={teaserDuoRight}
-        content="NEO CRAFT is a Berlin-based furniture label founded by designer Sebastian Scherer in 2015 , dedicated to blending traditional craftsmanship with modern production techniques. The label challenges conventional ideas about materials, form, and function, reinterpreting them to create innovative and unexpected designs. At the heart of NEO/CRAFT lies a distinctive approach to material exploration and development processes. While the current focus is on metal, glass, and wood, the pursuit of new directions and fresh perspectives remains a fundamental part of NEO/CRAFT's ethos."
-      />
+      <div className="bespoke__top">
+        {' '}
+        <TeaserDuo
+          left={teaserDuoLeft}
+          right={teaserDuoRight}
+          content="NEO CRAFT is a Berlin-based furniture label founded by designer Sebastian Scherer in 2015 , dedicated to blending traditional craftsmanship with modern production techniques. The label challenges conventional ideas about materials, form, and function, reinterpreting them to create innovative and unexpected designs. At the heart of NEO/CRAFT lies a distinctive approach to material exploration and development processes. While the current focus is on metal, glass, and wood, the pursuit of new directions and fresh perspectives remains a fundamental part of NEO/CRAFT's ethos."
+        />
+      </div>
 
-      <HeroSplit
-        showDivider={true} // mittlere Linie
-        bandDecor={null} // kein Farbbalken
-        rightAspect="780/940" // rechts hochkant
-        leftTopAspect="788/520" // links oben etwas breiter
-        leftBottomAspect="788/420" // links unten flacher Block
-        leftTop={<HoverImage image={heroSplitLeft} />}
-        leftBottom={<div className="hs-spacer hs-spacer--light" />}
-        right={<HoverImage image={heroSplitRight} />}
-      />
-      <TeaserDuo left={teaserDuoLeftBottom} right={teaserDuoRightBottom} />
+      <div className="bespoke__middle">
+        
+        <HeroSplit
+          showDivider={true} // mittlere Linie
+          bandDecor={null} // kein Farbbalken
+          rightAspect="780/940" // rechts hochkant
+          leftTopAspect="788/520" // links oben etwas breiter
+          leftBottomAspect="788/420" // links unten flacher Block
+          leftTop={<HoverImage image={heroSplitLeft} />}
+          leftBottom={<div className="hs-spacer hs-spacer--light" />}
+          right={<HoverImage image={heroSplitRight} />}
+        />
+      </div>
+
+      <div className="bespoke__bottom">
+        <TeaserDuo left={teaserDuoLeftBottom} right={teaserDuoRightBottom} />
+      </div>
     </div>
   );
 }
