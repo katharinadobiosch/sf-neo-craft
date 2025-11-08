e929acd4426248d6284900bd4ea25df778840026
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-11-08 19:13:06 +0100

    [pdp] fix bespoke site

--- app/patterns/Bespoke/index.jsx
+++ app/patterns/Bespoke/index.jsx
@@ -1,5 +1,5 @@
 import {TeaserDuo} from '../TeaserDuo';
-import {HeroSplit} from '../HeroSplit';
+import {HeroSplit_Poster} from '../HeroSplit';
 
 import bespokeImageLeft from './bespokeImageLeft.jpg';
 import bespokeImageRight from './bespokeImageRight.jpg';
@@ -13,11 +13,14 @@ export default function Bespoke() {
         right={bespokeImageRight}
         content="NEO CRAFT is a Berlin-based furniture label founded by designer Sebastian Scherer in 2015 , dedicated to blending traditional craftsmanship with modern production techniques. The label challenges conventional ideas about materials, form, and function, reinterpreting them to create innovative and unexpected designs. At the heart of NEO/CRAFT lies a distinctive approach to material exploration and development processes. While the current focus is on metal, glass, and wood, the pursuit of new directions and fresh perspectives remains a fundamental part of NEO/CRAFT's ethos."
       />
-      {/* <HeroSplit
-        left={bespokeImageLeft}
-        right={bespokeHeroImage}
-        content="GOBA is about simplicity with character—a piece that invites interaction and creates presence without overwhelming the space."
-      /> */}
+      <HeroSplit_Poster
+        // left={bespokeImageLeft}
+        // right={bespokeHeroImage}
+        // content="GOBA is about simplicity with character—a piece that invites interaction and creates presence without overwhelming the space."
+        graphicColor="#F6A94A"
+        quote="GOBA is about simplicity with character…"
+        rightImg={bespokeHeroImage}
+      />
     </>
   );
 }
