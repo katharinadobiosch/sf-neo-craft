f4e15d795fa304994075f1358307521cd35d3841
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-11-09 19:35:00 +0100

    [cart] first cart styling

--- app/patterns/Cart/CartMain.jsx
+++ app/patterns/Cart/CartMain.jsx
@@ -1,22 +1,86 @@
+// import {useOptimisticCart} from '@shopify/hydrogen';
+// import {Link} from 'react-router';
+// import {useAside} from '~/patterns/Aside';
+// import {CartLineItem} from '~/patterns/Cart/CartLineItem';
+// import {CartSummary} from '~/patterns/Cart/CartSummary';
+// import './cart.scss';
+
+// /**
+//  * The main cart component that displays the cart items and summary.
+//  * It is used by both the /cart route and the cart aside dialog.
+//  * @param {CartMainProps}
+//  */
+// export function CartMain({layout, cart: originalCart}) {
+//   // The useOptimisticCart hook applies pending actions to the cart
+//   // so the user immediately sees feedback when they modify the cart.
+//   const cart = useOptimisticCart(originalCart);
+
+//   const linesCount = Boolean(cart?.lines?.nodes?.length || 0);
+//   const withDiscount =
+//     cart &&
+//     Boolean(cart?.discountCodes?.filter((code) => code.applicable)?.length);
+//   const className = `cart-main ${withDiscount ? 'with-discount' : ''}`;
+//   const cartHasItems = cart?.totalQuantity ? cart.totalQuantity > 0 : false;
+
+//   return (
+//     <div className={className}>
+//       <CartEmpty hidden={linesCount} layout={layout} />
+//       <div className="cart-details">
+//         <div aria-labelledby="cart-lines">
+//           <ul>
+//             {(cart?.lines?.nodes ?? []).map((line) => (
+//               <CartLineItem key={line.id} line={line} layout={layout} />
+//             ))}
+//           </ul>
+//         </div>
+//         {cartHasItems && <CartSummary cart={cart} layout={layout} />}
+//       </div>
+//     </div>
+//   );
+// }
+
+// /**
+//  * @param {{
+//  *   hidden: boolean;
+//  *   layout?: CartMainProps['layout'];
+//  * }}
+//  */
+// function CartEmpty({hidden = false}) {
+//   const {close} = useAside();
+//   return (
+//     <div hidden={hidden}>
+//       <br />
+//       <p>
+//         Looks like you haven&rsquo;t added anything yet, let&rsquo;s get you
+//         started!
+//       </p>
+//       <br />
+//       <Link to="/collections" onClick={close} prefetch="viewport">
+//         Continue shopping →
+//       </Link>
+//     </div>
+//   );
+// }
+
+// /** @typedef {'page' | 'aside'} CartLayout */
+// /**
+//  * @typedef {{
+//  *   cart: CartApiQueryFragment | null;
+//  *   layout: CartLayout;
+//  * }} CartMainProps
+//  */
+
+// /** @typedef {import('storefrontapi.generated').CartApiQueryFragment} CartApiQueryFragment */
+
+// app/patterns/Cart/CartMain.jsx
 import {useOptimisticCart} from '@shopify/hydrogen';
-import {Link} from 'react-router';
-import {useAside} from '~/patterns/Aside';
 import {CartLineItem} from '~/patterns/Cart/CartLineItem';
 import {CartSummary} from '~/patterns/Cart/CartSummary';
 import './cart.scss';
 
-
-/**
- * The main cart component that displays the cart items and summary.
- * It is used by both the /cart route and the cart aside dialog.
- * @param {CartMainProps}
- */
 export function CartMain({layout, cart: originalCart}) {
-  // The useOptimisticCart hook applies pending actions to the cart
-  // so the user immediately sees feedback when they modify the cart.
   const cart = useOptimisticCart(originalCart);
 
-  const linesCount = Boolean(cart?.lines?.nodes?.length || 0);
   const withDiscount =
     cart &&
     Boolean(cart?.discountCodes?.filter((code) => code.applicable)?.length);
@@ -25,50 +89,20 @@ export function CartMain({layout, cart: originalCart}) {
 
   return (
     <div className={className}>
-      <CartEmpty hidden={linesCount} layout={layout} />
+      {/* Free Shipping Bar */}
+
+      {/* Inhalte */}
       <div className="cart-details">
         <div aria-labelledby="cart-lines">
-          <ul>
+          <ul className="cart-lines">
             {(cart?.lines?.nodes ?? []).map((line) => (
               <CartLineItem key={line.id} line={line} layout={layout} />
             ))}
           </ul>
         </div>
+
         {cartHasItems && <CartSummary cart={cart} layout={layout} />}
       </div>
     </div>
   );
 }
-
-/**
- * @param {{
- *   hidden: boolean;
- *   layout?: CartMainProps['layout'];
- * }}
- */
-function CartEmpty({hidden = false}) {
-  const {close} = useAside();
-  return (
-    <div hidden={hidden}>
-      <br />
-      <p>
-        Looks like you haven&rsquo;t added anything yet, let&rsquo;s get you
-        started!
-      </p>
-      <br />
-      <Link to="/collections" onClick={close} prefetch="viewport">
-        Continue shopping →
-      </Link>
-    </div>
-  );
-}
-
-/** @typedef {'page' | 'aside'} CartLayout */
-/**
- * @typedef {{
- *   cart: CartApiQueryFragment | null;
- *   layout: CartLayout;
- * }} CartMainProps
- */
-
-/** @typedef {import('storefrontapi.generated').CartApiQueryFragment} CartApiQueryFragment */
