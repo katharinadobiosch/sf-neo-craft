1a4cce311f0bb476a0510b571d8667cc06771b95
Katharina Dobiosch <katharina.dobiosch@bettwaren-shop.de>
2025-12-28 12:18:24 +0100

    [blocker] wip

--- env.d.ts
+++ env.d.ts
@@ -10,10 +10,12 @@ import type {createAppLoadContext} from '~/lib/context';
 declare global {
   const process: {env: {NODE_ENV: 'production' | 'development'}};
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface Env extends HydrogenEnv {}
 }
 
 declare module 'react-router' {
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface AppLoadContext
     extends Awaited<ReturnType<typeof createAppLoadContext>> {}
 
@@ -25,5 +27,6 @@ declare module 'react-router' {
     context: AppLoadContext;
   }
 
+  // eslint-disable-next-line @typescript-eslint/no-empty-object-type
   interface SessionData extends HydrogenSessionData {}
 }
